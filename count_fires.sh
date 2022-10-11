cadea194747c8371408c13c0952ae3be68c421de
Zehao Li <114265059+ZehaoLi666@users.noreply.github.com>
2022-10-11 12:12:57 -0700

    count_fires.sh
    Zehao Li's homework

--- count_fires.sh
+++ count_fires.sh
@@ -1,32 +1,31 @@
-#!/bin/bash -l
+#!/bin/bash
+#Step1:download the file and name it data1.csv
+curl https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::recent-large-fire-perimeters-5000-acres.csv > data1.csv
 
-# step to write
-# download the CSV file
-# curl -o calfire.csv ...
-
-# print out the range of years found -- you may need to go in and edit the file
-# cut -d, ....
-MINYEAR=1900
-MAXYEAR=1901
-# write code to set these variables with the smallest and largest years
-echo "This report has the years: $MINYEAR-$MAXYEAR"
-# if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
-
-# print out the total number of fires (remember to remove the header line)
-TOTALFILECOUNT=0
-# put your code here to update this variable
-echo "Total number of files: $TOTALFILECOUNT"
-
-# print out the number of fire in each year
-echo "Number of fires in each year follows:"
+#Step2:Print out the range of years:
+#cut the second field and discover the wired number, so repair it firstly. Then cut the second field and sort this field 
+sed -e 's/\r//g' data1.csv | sed 'N;s/\n"//;P;D' | sed "s/\".*\"//" >data2.csv
+    cut -d, -f2 data2.csv | sort |uniq > data2_1.csv
+    echo "The range of years is" $(awk 'NR==1' data2_1.csv) "to" $(awk 'NR==5' data2_1.csv)"." 
 
 
-# print out the name of the largest file use the GIS_ACRES and report the number of acres
-echo "Largest fire was $LARGEST and burned $LARGESTACRES"
+#Step3:Print out the number of fires in the database
+awk '{if(NR > 1) {print $0}}' data2.csv > data3.csv 
+echo "The number of fires is" $(cat data3.csv|wc -l)"."
 
-# print out the years - change the code in $(echo 1990) to print out the years (hint - how did you get MINYEAR and MAXYEAR?
-for YEAR in $(echo 1990)
-do
-#      TOTAL=$(grep ... | awk ...)
-      echo "In Year $YEAR, Total was $TOTAL"
-done
+#Step4: Print out the number of fires that occur each year
+cut -d, -f2 data3.csv | sort |uniq -c > count.csv  
+echo "The number of fires that occur from 2017 to 2021 is"$(awk '{print $1}' count1.csv)"respectively."
+
+#Step5:Print out the name largest fire
+cut -d,  -f6,13 data3.csv >name.csv
+sort -t, -k2 -n name.csv >name1.csv
+echo "The name of the largest fire is" $(awk 'NR==134' name1.csv)"."
+
+#Step6:Print out the total acreage burned in each year
+cut -d,  -f2,13 data3.csv >acreage.csv 
+echo "The total acreage burned in each year are "$(awk '{if(NR > 1) {print $0}}' acreage.csv | awk -F'[,]' '{a[$1]+=$2}END{for(i in a) print i","a[i]}') "acres."
+
+
+
+
